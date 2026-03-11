2072fa74ffc16adfc5a2bc5f879db71617917233
Chinnakrit nualsri <chinnakrit@adda.co.th>
2026-03-11 16:33:41 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-5xl font-bold text-blue-500">Hello Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
