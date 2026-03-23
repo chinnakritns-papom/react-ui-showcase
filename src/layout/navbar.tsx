e240a5c0a99732f3aaa516c01f2618f36251e00b
Chinnakrit nualsri <chinnakrit@adda.co.th>
2026-03-23 16:24:51 +0700

    update

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -8,7 +8,7 @@ import {
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BellIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
